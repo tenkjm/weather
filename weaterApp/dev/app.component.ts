7cccbb81d96b3eb047fb0905b101b8d60536b57d
Anton <atour07@yandex.ru>
2017-08-26 11:27:48 +0400

    Code added

---
+++ weaterApp/dev/app.component.ts
@@ -0,0 +1,16 @@
+import {Component} from 'angular2/core';
+import {WeatherListComponent} from "./weather/weather-list.component"
+
+@Component({
+    selector: 'my-app',
+    template: `
+        <header>
+            <h1>Angular 2 Weather</h1>
+        </header>
+        <weather-list></weather-list>
+    `,
+    directives:[WeatherListComponent]
+})
+export class AppComponent {
+
+}
---
+++ weaterApp/dev/weather/weather-item.component.ts
@@ -0,0 +1,23 @@
+import {Component, Input} from "angular2/core";
+import {WeatherItem} from "./weather-item";
+
+@Component({
+    selector: 'weather-item',
+    template:`
+    <article class="weather-element">
+        <div class="col-1">
+            <h3>{{ weatherItem.cityName }}</h3>
+            <p class="info">{{ weatherItem.description }}</p>
+        </div>
+        <div class="col-2">
+            <span class="temperature">{{ weatherItem.temperature }}°C</span>
+        </div>
+    </article>`,
+    styleUrls:['src/css/weather-item.css']
+
+})
+
+
+export class WeatherItemComponent{
+    @Input('item') weatherItem: WeatherItem;
+}
